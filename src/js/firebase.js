dd52bd6cf8e7fd34713f6785d1bb7a96e2f77396
Noufer Mohamed A <90058162+rovannoufer@users.noreply.github.com>
2024-06-19 19:25:03 +0530

    Update firebase.js

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCknQtWGq_M1WKEZ91sJ4k2-0f_d3JMFnQ",
-  authDomain: "detection-2de0e.firebaseapp.com",
-  projectId: "detection-2de0e",
-  storageBucket: "detection-2de0e.appspot.com",
-  messagingSenderId: "726952764140",
-  appId: "1:726952764140:web:943326300f22d182893bc9",
-  measurementId: "G-XPF08BTHZ9"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
